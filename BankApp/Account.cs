07087510491cc5bffffdb81c7e34598d91cc207c
SyahinHussein <45961879+SyahinHussein@users.noreply.github.com>
2019-09-27 17:42:11 -0700

    Initial commit

---
+++ BankApp/Account.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using System.Text;
+
+namespace BankApp
+{
+    /// <summary> triple slash
+    /// This is a bank account
+    /// where a user can deposit 
+    /// and withdarw money from it
+    /// </summary>
+    class Account
+    {
+        private static int lastAccountNumber = 0;
+
+      
+
+        #region Properties
+        /// <summary>
+        /// Email Address of the account
+        /// </summary>
+        public string EmailAddress { get; set; } 
+
+        /// <summary>
+        /// Autogenerate Account number 
+        /// for the account
+        /// </summary>
+        public int AccountNumber { get; } //set; } 
+
+        /// <summary>
+        /// This sets the account type
+        /// </summary>
+        public string AccountType { get; set; } 
+
+        /// <summary>
+        /// This checks the account balance
+        /// </summary>
+        public decimal Balance { get; private set; } 
+
+        /// <summary>
+        /// This checks the created date of the bank account
+        /// </summary>
+        public DateTime CreatedDate { get; private set; }
+        #endregion//this is good t0 keep the code clean
+
+        #region Constructor
+        public Account()
+        {
+            
+            AccountNumber = ++lastAccountNumber;
+            CreatedDate = DateTime.Now;
+        }
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// Deposit money into the account
+        /// </summary>
+        /// <param name="amount">Amount to deposit</param>
+        public void Deposit(decimal amount)
+        {
+            Balance += amount;
+        }
+
+        #endregion
+    }
+}
+
---
+++ BankApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace BankApp
+{
+    class Program
+    {
+         
+        static void Main(string[] args)//Main is the entry
+        {
+            //Instance of an object ==object
+            //...properties of object
+            var account = new Account
+            {
+                EmailAddress = "test@test.com",
+                AccountType = "Checking"
+
+            };
+
+            account.Deposit(2034.56M);
+
+            Console.WriteLine($"AN:{account.AccountNumber}, " +
+                $"Balance: {account.Balance:C}, " +
+                $"AT:{account.AccountType}, " +
+                $"EA: {account.EmailAddress}, " +
+                $"CD:{account.CreatedDate}");
+        
+        }
+
+    }
+}
